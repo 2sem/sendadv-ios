9bda4b849019ff2ba6414bc971b470a157cab114
leesam <toyboy2@hanmail.net>
2025-03-10 22:51:02 +0900

    refactor: apply tuist

---
+++ Tuist.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+
+let tuist = Tuist(
+    project: .tuist(compatibleXcodeVersions: .upToNextMajor("16.0")
+//                    swiftVersion: "",
+//                    plugins: <#T##[PluginLocation]#>,
+//                    generationOptions: <#T##Tuist.GenerationOptions#>,
+//                    installOptions: <#T##Tuist.InstallOptions#>)
+    )
+)
---
+++ Tuist/ProjectDescriptionHelpers/String+.swift
@@ -0,0 +1,12 @@
+//
+//  String+.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 영준 이 on 3/9/25.
+//
+
+import Foundation
+
+public extension String {
+    static var appBundleId : String { "com.credif.sendadv" }
+}
