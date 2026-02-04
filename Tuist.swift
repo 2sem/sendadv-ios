fb2618f0ca7c59b67ae2a51dd0138ffa8cb85bda
leesam <toyboy2@hanmail.net>
2026-02-04 22:50:37 +0900

    ci: add tuist handle

--- Tuist.swift
+++ Tuist.swift
@@ -1,6 +1,7 @@
 import ProjectDescription
 
 let tuist = Tuist(
+    fullHandle: "gamehelper/sendadv",
     project: .tuist(compatibleXcodeVersions: .upToNextMajor("26.0")
 //                    swiftVersion: "",
 //                    plugins: <#T##[PluginLocation]#>,
