e48f5b0b4d14e8d08e86761c97dddc81daf0b13a
leesam <toyboy2@hanmail.net>
2026-02-05 20:52:58 +0900

    fix: syntax error for new extension of UIApplication

--- Projects/App/Sources/Extensions/UIApplication+RootViewController.swift
+++ Projects/App/Sources/Extensions/UIApplication+RootViewController.swift
@@ -11,11 +11,10 @@ import UIKit
 extension UIApplication {
 	/// foregroundActive 씨너에서 키 윈도우의 rootViewController를 반환합니다.
 	/// UIWindowScene을 직접 사용하는 보일러플레이트를 제거하기 위한 편의 속성입니다.
-	var topRootViewController: UIViewController? {
-		(connectedScenes
-			.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene)
-			?.windows
-			.first(where: { $0.isKeyWindow })
-			?.rootViewController
-	}
-}
+	    var topRootViewController: UIViewController? {
+	        guard let windowScene = connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene else {
+	            return nil
+	        }
+	        
+	        return windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController
+	    }}
