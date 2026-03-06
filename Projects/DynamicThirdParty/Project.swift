9e7e59edf61d3841c8f674e1ba92856faae2280c
leesam <toyboy2@hanmail.net>
2026-03-06 14:55:56 +0900

    chore: migrate SDWebImage and Firebase to Tuist registry
    - Enable registryEnabled: true in Tuist.swift
    - Migrate SDWebImage → sdwebimage.sdwebimage
    - Migrate firebase-ios-sdk → firebase.firebase-ios-sdk
    - ThirdParty packages remain as .remote (not on Swift Package Index registry)

--- Projects/DynamicThirdParty/Project.swift
+++ Projects/DynamicThirdParty/Project.swift
@@ -3,10 +3,8 @@ import ProjectDescriptionHelpers
 
 let project = Project(
     name: "DynamicThirdParty",
-    packages: [.remote(url: "https://github.com/SDWebImage/SDWebImage.git",
-                       requirement: .upToNextMajor(from: "5.1.0")),
-               .remote(url: "https://github.com/firebase/firebase-ios-sdk",
-                       requirement: .upToNextMajor(from: "11.8.1")),
+    packages: [        .package(id: "sdwebimage.sdwebimage", from: "5.1.0"),
+                       .package(id: "firebase.firebase-ios-sdk", from: "11.8.1"),
     ],
     targets: [
         .target(
